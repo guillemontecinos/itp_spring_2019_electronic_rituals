4647b5d73eef20b6955bec91b4a6d4155c0d06fd
gamontec <gamontec@uc.cl>
2019-05-01 10:24:03 -0400

    connected to database

--- final/code/index.js
+++ final/code/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const mustacheExpress = require('mustache-express');
+const { Client } = require('pg')
 
 let app = express()
 app.engine('html', mustacheExpress());
@@ -9,10 +10,19 @@ app.set('view engine', 'html');
 app.set('views', __dirname);
 app.use(bodyParser.urlencoded({ extended: false }))
 
+let client = new Client({database: 'mapmemory'})
+client.connect()
+
+// storage variables
+let lat = 0
+let lon = 0
+let memory = ''
+let address = ''
+
 app.get('/', function(req, res){
     // TODO: add numeric input confirmation
-    let lat = req.query.lat_0
-    let lon = req.query.lon_0
+    lat = req.query.lat_0
+    lon = req.query.lon_0
 
     if(lat === undefined || lon === undefined){
         res.sendFile(path.join(__dirname + '/index.html'))
@@ -25,15 +35,24 @@ app.get('/', function(req, res){
 
 app.post('/memory', function(req, res){
     // TODO: add input confirmation
-    let memory = req.body.memory
+    memory = req.body.memory
     console.log('Memory: ' + memory)
     res.sendFile(path.join(__dirname + '/memtwo.html'))
 })
 
 app.get('/memory', function(req, res){
     // TODO: add input confirmation
-    let address = req.query.address
+    address = req.query.address
     console.log('Address: ' + address)
+
+    // post on database
+    client.query('INSERT INTO posts (lat1, lon1, memory, address) VALUES ($1, $2, $3, $4)',[lat, lon, memory, address], (err, res) => {
+        if (err) {
+          console.log(err.stack)
+        } else {
+            console.log('posted successfully')
+        }
+      })
     
     // pass variable from express to client-side js
     // res.render('/memtwo', function(req, res){
